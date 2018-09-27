61c7414197a62cf2f62b7ff5daf42a7ce4fe1b84
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-27 15:40:37 -0700

    Adding SB Admin html structure to the dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,33 +1,67 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <!-- meta tags -->
-        @include("layouts.partials.metas") 
-        
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" href="/imgs/favicon/favicon.png">
-        
-        <!-- Title -->
-        <title>{{ env("APP_NAME") }}</title>
-
-        <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="{{ mix('/css/dashboard/app.css') }}">
-    </head>
-    <body>
-        <div id="app">
-            <!-- navbar -->
-            @include("layouts.partials.navbar")
-
-            <!-- Content -->
-            <div class="content">
-                @yield("content")
+   <head>
+      <!-- meta tags -->
+      @include("layouts.partials.metas") 
+      <!-- Favicon -->
+      <link rel="icon" type="image/png" href="/imgs/favicon/favicon.png">
+      <!-- Title -->
+      <title>{{ env("APP_NAME") }}</title>
+      <!-- Styles -->
+      <link rel="stylesheet" type="text/css" href="{{ mix('/css/dashboard/app.css') }}">
+   </head>
+   <body id="page-top">
+      <div id="app">
+      @include('layouts.partials.navbar-dashboard')
+      <div id="wrapper">
+         <!-- Sidebar -->
+         <ul class="sidebar navbar-nav bg-light">
+            <li class="nav-item">
+               <a class="nav-link" href="">
+               <i class="fas fa-fw fa-tachometer-alt"></i>
+               <span>Dashboard</span>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="">
+               <i class="fas fa-fw fa-chart-area"></i>
+               <span>Charts</span></a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="">
+               <i class="fas fa-fw fa-table"></i>
+               <span>Tables</span></a>
+            </li>
+         </ul>
+         <div id="content-wrapper">
+            <div class="container-fluid">
+               <!-- Breadcrumbs-->
+               <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                     <a href="">Dashboard</a>
+                  </li>
+                  <li class="breadcrumb-item active">Blank Page</li>
+               </ol>
+               <!-- Page Content -->
+                <div class="content">
+                    @yield("content")
+                </div>
             </div>
-            @include('layouts.partials.form-error')
-
-            @include('layouts.partials.footer')
-        </div>
-
-        <script type="text/javascript" src="{{ mix('/js/dashboard/app.js') }}"></script>
-    </body>
+            <!-- /.container-fluid -->
+            <!-- Sticky Footer -->
+            @include('layouts.partials.footer-dashboard')
+         </div>
+         <!-- /.content-wrapper -->
+      </div>
+      <!-- /#wrapper -->
+      <!-- Scroll to Top Button-->
+      <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+      </a>
+      <!-- Logout Modal-->
+      @include('layouts.partials.logout-modal')
+      </div>
+      <!-- Bootstrap core JavaScript-->
+      <script type="text/javascript" src="{{ mix('/js/dashboard/app.js') }}"></script>
+   </body>
 </html>
-
