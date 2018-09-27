235ed6a465aee01e19546486ffa4449aed639cd1
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-27 15:35:26 -0700

    Adding dashboard layout partials

---
+++ resources/views/layouts/partials/footer-dashboard.blade.php
@@ -0,0 +1,18 @@
+<footer class="sticky-footer bg-light">
+   <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <h3 class="text-center">
+            <a href="https://www.linkedin.com/in/thiago-mallon/" data-social="linkedin" class="text-primary" target="_blank">
+                <i class="fab fa-linkedin text-primary"></i>
+            </a>
+            <a href="https://github.com/jamesmallon" data-social="github" class="text-dark" target="_blank">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://plus.google.com/u/0/115970027515273968388?tab=mX" data-social="google" class="text-danger" target="_blank">
+                <i class="fab fa-google-plus-g text-danger"></i>
+            </a>
+         </h3>
+         <!--  <span>Copyright © Your Website 2018</span> -->
+      </div>
+   </div>
+</footer>
