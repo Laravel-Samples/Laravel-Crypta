f5907c0c05d05f3fa8e9e01a7e6d825d499561b8
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-30 17:57:27 -0700

    webpack mix

--- public/js/dashboard/app.js
+++ public/js/dashboard/app.js
@@ -1783,6 +1783,62 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?{\"cacheDirectory\":true,\"presets\":[[\"env\",{\"modules\":false,\"targets\":{\"browsers\":[\"> 2%\"],\"uglify\":true}}],\"react\"],\"plugins\":[\"transform-object-rest-spread\",[\"transform-runtime\",{\"polyfill\":false,\"helpers\":false}]]}!./node_modules/vue-loader/lib/selector.js?type=script&index=0!./resources/js/dashboard/components/UpdateHomeMessage.vue":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    data: function data() {
+        return {
+            newMessage: '',
+            feedbackBox: '',
+            invalidRes: false
+        };
+    },
+
+    methods: {
+        save: function save() {
+            if (this.newMessage.length) {
+                axios.post('/ajax/send-message', { message: this.newMessage }).catch(function (error) {
+                    return error.response;
+                }).then(this.setResponse);
+
+                this.newMessage = '';
+            }
+        },
+        setResponse: function setResponse(response) {
+            if (response.status != 200) {
+                this.invalidRes = true;
+                this.feedbackBox = response.data.errors.message[0];
+            } else {
+                this.invalidRes = false;
+                this.feedbackBox = "Your message was sent!";
+            }
+        }
+    }
+});
+
+/***/ }),
+
 /***/ "./node_modules/backo2/index.js":
 /***/ (function(module, exports) {
 
@@ -45658,6 +45714,86 @@ module.exports = function normalizeComponent (
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/template-compiler/index.js?{\"id\":\"data-v-3fe23bdf\",\"hasScoped\":false,\"buble\":{\"transforms\":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./resources/js/dashboard/components/UpdateHomeMessage.vue":
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", [
+    _c("div", { staticClass: "form-row" }, [
+      _c("div", { staticClass: "col" }, [
+        _c("label", { attrs: { for: "transfer-username" } }, [
+          _vm._v("Home Message")
+        ]),
+        _vm._v(" "),
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.newMessage,
+              expression: "newMessage"
+            }
+          ],
+          staticClass: "form-control",
+          class: { "is-invalid": _vm.invalidRes },
+          attrs: {
+            type: "text",
+            placeholder: "Write here your home message...",
+            autofocus: ""
+          },
+          domProps: { value: _vm.newMessage },
+          on: {
+            keydown: function($event) {
+              if (
+                !("button" in $event) &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.save($event)
+            },
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.newMessage = $event.target.value
+            }
+          }
+        }),
+        _vm._v(" "),
+        _c("div", {
+          class: { "invalid-feedback": _vm.invalidRes },
+          domProps: { innerHTML: _vm._s(_vm.feedbackBox) }
+        })
+      ])
+    ]),
+    _vm._v(" "),
+    _c(
+      "button",
+      {
+        staticClass: "btn btn-primary mt-4",
+        attrs: { type: "button" },
+        on: { click: _vm.save }
+      },
+      [_c("strong", [_vm._v("Send!")])]
+    )
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-3fe23bdf", module.exports)
+  }
+}
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/template-compiler/index.js?{\"id\":\"data-v-4c062f13\",\"hasScoped\":false,\"buble\":{\"transforms\":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./resources/js/dashboard/components/TransferSent.vue":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -56964,6 +57100,7 @@ window.Vue = __webpack_require__("./node_modules/vue/dist/vue.common.js");
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.component('home-message-block', __webpack_require__("./resources/js/dashboard/components/UpdateHomeMessage.vue"));
 Vue.component('transfer-block', __webpack_require__("./resources/js/dashboard/components/TransferSent.vue"));
 
 var app = new Vue({
@@ -57035,6 +57172,54 @@ if (false) {(function () {
     hotAPI.createRecord("data-v-4c062f13", Component.options)
   } else {
     hotAPI.reload("data-v-4c062f13", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ "./resources/js/dashboard/components/UpdateHomeMessage.vue":
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+var normalizeComponent = __webpack_require__("./node_modules/vue-loader/lib/component-normalizer.js")
+/* script */
+var __vue_script__ = __webpack_require__("./node_modules/babel-loader/lib/index.js?{\"cacheDirectory\":true,\"presets\":[[\"env\",{\"modules\":false,\"targets\":{\"browsers\":[\"> 2%\"],\"uglify\":true}}],\"react\"],\"plugins\":[\"transform-object-rest-spread\",[\"transform-runtime\",{\"polyfill\":false,\"helpers\":false}]]}!./node_modules/vue-loader/lib/selector.js?type=script&index=0!./resources/js/dashboard/components/UpdateHomeMessage.vue")
+/* template */
+var __vue_template__ = __webpack_require__("./node_modules/vue-loader/lib/template-compiler/index.js?{\"id\":\"data-v-3fe23bdf\",\"hasScoped\":false,\"buble\":{\"transforms\":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./resources/js/dashboard/components/UpdateHomeMessage.vue")
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/js/dashboard/components/UpdateHomeMessage.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-3fe23bdf", Component.options)
+  } else {
+    hotAPI.reload("data-v-3fe23bdf", Component.options)
   }
   module.hot.dispose(function (data) {
     disposed = true
