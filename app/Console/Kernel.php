774baeaf3ff72554d02912806f566f3ed0914dc4
Thiago Mallon <jamesmallondev@gmail.com>
2018-10-01 22:30:35 -0700

    indentation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,18 +40,10 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule The Schedule object.
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
